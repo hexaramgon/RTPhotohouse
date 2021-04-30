84167ea966c4f515d3d923c3b7022cd58bba4f10
Hector Ramos <hector.ramos@berkeley.edu>
2021-04-29 20:04:48 -0700

    V2.2

--- src/App.js
+++ src/App.js
@@ -7,23 +7,23 @@ import Basis from './Basis';
 
 class App extends React.Component {
 
-  // authenticate(){
-  //   return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
-  // }
+  authenticate(){
+    return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
+  }
 
-  // componentDidMount(){
-  //   this.authenticate().then(() => {
-  //     const ele = document.getElementById('ipl-progress-indicator')
-  //     if(ele){
-  //       // fade out
-  //       ele.classList.add('available')
-  //       setTimeout(() => {
-  //         // remove from DOM
-  //         ele.outerHTML = ''
-  //       }, 2000)
-  //     }
-  //   })
-  // }
+  componentDidMount(){
+    this.authenticate().then(() => {
+      const ele = document.getElementById('ipl-progress-indicator')
+      if(ele){
+        // fade out
+        ele.classList.add('available')
+        setTimeout(() => {
+          // remove from DOM
+          ele.outerHTML = ''
+        }, 2000)
+      }
+    })
+  }
 
   render() { 
   return (
